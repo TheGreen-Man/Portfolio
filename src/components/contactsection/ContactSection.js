1e8ff97455e0f07fac747cd18b3242fcd364689c
TheGreen-Man <af18424@alas.matf.bg.ac.rs>
2022-03-01 21:43:10 +0100

    Completed

--- src/components/contactsection/ContactSection.js
+++ src/components/contactsection/ContactSection.js
@@ -42,7 +42,10 @@ export default function ContactSection() {
 				<a href="mailto:crncevicf@gmail.com?subject=Hello!">
 					<FaRegEnvelopeOpen className="icon" />
 				</a>
-				<a href="http://filipcrncevic.rocks/Crncevic_CV.pdf" download>
+				<a
+					href="https://filipcrncevic.rocks/Crncevic_CV_2022.pdf"
+					download="Crncevic_CV_2022.pdf"
+				>
 					<FaRegFilePdf className="icon" />
 				</a>
 			</div>
